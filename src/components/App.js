858ef106cb832557e0fb8a663c7fdf7ed2445b89
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-22 20:28:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+
+  const [name, setName] = useState("");
+
+  
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label htmlFor="nameInput">Enter your name:</label>
+      <input
+        id="nameInput"
+        type="text"
+        value={name}
+        onChange={handleNameChange}
+      />
+      {/* Display the greeting only if `name` is not empty */}
+      {name && <p>Hello, {name}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
